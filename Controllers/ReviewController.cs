e59dee345a1755f1e06fc7f75b22b67e091c1eeb
AlexandruTerescenco <alexandruterescencp@gmail.com>
2025-02-20 23:37:44 +0200

    Add project files.

---
+++ Controllers/ReviewController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proiect_Final_TerescencoAlexandru.DB;
+using Proiect_Final_TerescencoAlexandru.Models;
+
+namespace Proiect_Final_TerescencoAlexandru.Controllers
+{
+    public class ReviewController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ReviewController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //1. Frostpunk 2 example page
+        public ActionResult Review()
+        {
+            return View();
+        }
+    }
+}
